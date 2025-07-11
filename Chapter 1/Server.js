92d58646ba4243bcce18a3d50078206020147be6
ROHIT DUBEY <rohitdubey713010@gmail.com>
2025-07-11 15:45:56 +0530

    Working on Basics of nodejs

---
+++ Chapter 1/Server.js
@@ -0,0 +1,26 @@
+console.log('Hello World')
+// console.log(global);
+
+// const info = require('os');
+// const path = require('path');
+const {add,substract,multiply,divide}  = require('./math')
+
+console.log(add(2,5),divide(5,2),multiply(5,2),substract(7,1))
+
+
+// console.log(info.type())
+// console.log(info.version())
+// console.log(info.homedir())
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+// console.log(path.parse(__filename))
+
+
---
+++ Chapter2/index.js
@@ -0,0 +1,62 @@
+const fspromises = require('fs').promises;
+const path = require('path')
+
+const funct = async() => {
+    try{
+      const newdata = await  fspromises.readFile(path.join(__dirname,'files','starter.txt'),'utf8')
+      console.log(newdata)
+       await fspromises.writeFile(path.join(__dirname,'files','Promiseswrite.txt') , newdata)
+       await fspromises.appendFile(path.join(__dirname,'files','Promiseswrite.txt'),'This is append file ')
+       await fspromises.rename(path.join(__dirname,'files','Promiseswrite.txt'),path.join(__dirname,'files','Promise.txt'),'This is rename file ')
+       
+       const newdata1 = await fspromises.readFile(path.join(__dirname,'files','Promise.txt'),'utf8') 
+       console.log(newdata1)
+
+    
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+} 
+
+funct();
+// fs.readFile(path.join(__dirname,'files','starter.txt'),'utf8',(err,data) =>
+//  {
+//      if ( err) throw err ;
+//      console.log(data.toString());
+//  } )
+
+//  console.log('HEllo.....')
+
+
+// fs.writeFile(path.join(__dirname,'files','reply.txt'),'Nice to meet you ',(err) =>
+//  {
+//      if ( err) throw err ;
+//     console.log('Write Complete ')
+
+//     fs.appendFile(path.join(__dirname,'files','reply.txt'),'I create this filename as append',(err) =>
+//  {
+//      if ( err) throw err ;
+//     console.log('append Complete ')
+//  } )
+//     fs.rename(path.join(__dirname,'files','reply.txt'),'I create this filename as append',(err) =>
+//  {
+//      if ( err) throw err ;
+//     console.log('rename Complete ')
+//  } )
+
+//  } )
+
+
+
+
+
+
+
+
+// //error handling
+// process.on('uncaughtException',err =>{
+//     console.error('There was Error while executing ',err)
+//     process.exit(1);
+// })
